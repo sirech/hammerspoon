a5c1cd1a0fbd2b60fd1c3ed701b464b4e4f6c0be
Mario Fernandez <mario.fernandez@hceris.com>
2024-08-02 23:54:03 +0200

    update triggers

--- modules/triggers.lua
+++ modules/triggers.lua
@@ -6,27 +6,21 @@ local mouse = require 'hs.mouse'
 
 local function module_init()
     triggers = {
-        { "1", "/Applications/Emacs.app/Contents/MacOS/Emacs-x86_64-10_14"},
+        { "1", "Emacs"},
         { "2", "iTerm"},
-        { "3", "Brave Browser"},
-        { "4", "/Applications/Chromium.app/Contents/MacOS/Chromium"},
+        { "3", "Google Chrome Canary"},
+        { "4", "Google Chrome"},
         { "5", "firefox"},
         { "6", "Safari"},
-        { "7", "Google Chrome Canary" },
         { "M", "Google Mail"},
-        { "H", "Chat"},
-        { "J", "Slack"},
+        { "H", "Workplace Chat"},
         { "C", "Google Calendar"},
-        { "X", "xScope"},
         { "T", "Whale"},
         { "K", "KeePassXC"},
         { "N", "Notes"},
         { "I", "IntelliJ IDEA"},
         { "V", "Visual Studio Code"},
-        { "B", "Basecamp 3"},
         { "G", "zoom.us" },
-        { "P", "Postman" },
-        { "U", "Microsoft Teams"}
     }
 
     for _, trigger in ipairs(triggers) do
